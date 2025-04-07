5d75dd385610028e5eb6eee649fa27fddf964f80
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-04-07 20:03:27 +0200

    Added consts and arithmetics

---
+++ basics/constants.go
@@ -0,0 +1,22 @@
+package basics
+
+import "fmt"
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const DAYS_WEEK int = 7
+
+	const (
+		monday    = 1
+		tuesday   = 2
+		wednesday = 3
+		thursday  = 4
+		friday    = 5
+		saturday  = 6
+		sunday    = 7
+	)
+
+	fmt.Println("The number PI is:", PI)
+}
