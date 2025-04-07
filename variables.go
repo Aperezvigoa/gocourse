1eb45d5f03cec912f298d997f3a0f200824bada4
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-04-07 16:43:47 +0200

    Learned imports, variables and data types

---
+++ variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var middleName string = "Calonge"
+
+func main() {
+	// var age int
+	// var firstCatName string = "Cheddar"
+	// var secondCatName string = "Bigotitos"
+
+	// count := 10
+	// lastName := "Albizu"
+
+	// Default Values
+	// Numeric Types: 0
+	// Boolean:  false
+	// String type: ""
+	// Pointers, slices, maps, functions and structs: nil
+
+	// ------------ SCOPE
+	middleName := "Perez-Vigo"
+	fmt.Println(middleName)
+}
+
+func printName() {
+	firstName := "Lilui"
+	fmt.Println(firstName)
+}
